5bccf47ab59f2d6ab96184ad109c5608667c790e
Mateo Fuzul <mfuzul@hotmail.com>
2022-03-18 22:25:41 +0100

    feat: add GraphQL to project

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,24 @@
 import express from 'express';
+import { graphqlHTTP } from 'express-graphql';
+import { buildSchema } from 'graphql';
 
 const PORT = 8080;
 const HOST = '0.0.0.0';
 
+const schema = buildSchema(`
+    type Query {
+        hello: String
+    }
+`);
+
+const root = { hello: () => 'Hello world' };
+
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Hello world');
-});
+app.use('/graphql', graphqlHTTP({
+    schema,
+    rootValue: root,
+    graphiql: false,
+}));
 
 app.listen(PORT, HOST);
